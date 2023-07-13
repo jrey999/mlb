4f297285f897e957c16cbb793e8731c08e017366
jgr1204 <reynoldsjohngreg@gmail.com>
2023-07-13 19:53:36 -0400

    log tables that have been created

--- schema.sh
+++ schema.sh
@@ -2,5 +2,14 @@
 folder="$(dirname "$0")/data/schema"
 
 for file in "$folder"/*; do
-    sqlite3 db.sqlite3 < $file
+    tablename="${file##*/}"
+    tablename="${tablename%.sql}"  # Remove the ".sql" extension to get the table name
+    
+    # Check if the table exists
+    if sqlite3 your_database.db ".table $tablename" &>/dev/null; then
+        echo "Table $tablename already exists"
+    else
+        sqlite3 db.sqlite3 < $file
+        echo "Table $tablename has been created"
+    fi
 done
